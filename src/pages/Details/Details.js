945a41c816a1e4197b3ea1a3421f1511221caa24
Ashish Talati <ashish.talati@gmail.com>
2021-09-16 11:59:23 -0500

    Update rating

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
-import Rating from '@material-ui/lab/Rating';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import axios from 'axios';
 
